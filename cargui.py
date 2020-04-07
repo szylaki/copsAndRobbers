b1af1e3295a29f92f3dc60d9c58a0ed024b8ea81
Igor Szylak <50166702+szylaki@users.noreply.github.com>
2020-04-07 17:25:16 +0200

    Add files via upload

---
+++ cargui.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class Gui(QWidget):
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        #self.n = n
+        self.interface()
+        self.grid = True
+        
+    def interface(self):
+        
+        self.resize(1024, 768)
+        self.setWindowTitle("Cops and Robbers")
+        self.show()
+        
+    #def createGrid():
+        #grid = [[for x in range()]]
+        
+if __name__ == '__main__':
+    import sys
+    
+    app = QApplication(sys.argv)
+    okno = Gui()
+    sys.exit(app.exec_())
